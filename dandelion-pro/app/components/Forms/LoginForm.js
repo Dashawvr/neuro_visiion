61a8e15775039ef6ba5845b94227fb3544385a2e
Dashawvr <dasha.onishchenko@gmail.com>
2021-02-03 13:36:30 +0200

    FIX: LoginForm

--- dandelion-pro/app/components/Forms/LoginForm.js
+++ dandelion-pro/app/components/Forms/LoginForm.js
@@ -1,11 +1,11 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import {withStyles} from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import classNames from 'classnames';
-import {Field, reduxForm} from 'redux-form/immutable';
+import { Field, reduxForm } from 'redux-form/immutable';
 import Button from '@material-ui/core/Button';
-import {connect} from 'react-redux';
-import {NavLink} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import IconButton from '@material-ui/core/IconButton';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
@@ -20,9 +20,9 @@ import Paper from '@material-ui/core/Paper';
 import Hidden from '@material-ui/core/Hidden';
 import brand from 'dan-api/dummy/brand';
 import logo from 'dan-images/logo.svg';
-import {TextFieldRedux, CheckboxRedux} from './ReduxFormMUI';
+import { TextFieldRedux, CheckboxRedux } from './ReduxFormMUI';
 import styles from './user-jss';
-import {ContentDivider} from '../Divider';
+import { ContentDivider } from '../Divider';
 
 // validation functions
 const required = value => (value === null ? 'Required' : undefined);
@@ -33,162 +33,155 @@ const required = value => (value === null ? 'Required' : undefined);
 // );
 
 const LinkBtn = React.forwardRef(function LinkBtn(props, ref) { // eslint-disable-line
-    return <NavLink to={props.to} {...props} innerRef={ref}/>; // eslint-disable-line
+  return <NavLink to={props.to} {...props} innerRef={ref}/>; // eslint-disable-line
 });
 
 // eslint-disable-next-line
 class LoginForm extends React.Component {
-    state = {
-        showPassword: false,
-        email: '',
-        password: ''
-    }
+  state = {
+    showPassword: false,
+    email: '',
+    password: ''
+  }
 
-    handleClickShowPassword = () => {
-        const {showPassword} = this.state;
-        this.setState({showPassword: !showPassword});
-    };
+  handleClickShowPassword = () => {
+    const { showPassword } = this.state;
+    this.setState({ showPassword: !showPassword });
+  };
 
-    handleMouseDownPassword = event => {
-        event.preventDefault();
-    };
+  handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
 
-    render() {
-        const {
-            classes,
-            handleSubmit,
-            pristine,
-            submitting,
-            deco,
-            onChangeEmail,
-            onChangePassword,
-            password,
-            email
-        } = this.props;
-        const {showPassword} = this.state;
-        return (
-            <Fragment>
-                <Hidden mdUp>
-                    <NavLink to="/" className={classNames(classes.brand, classes.outer)}>
-                        <img src={logo} alt={brand.name}/>
-                        Neuro Vision
-                    </NavLink>
-                </Hidden>
-                <Paper className={classNames(classes.paperWrap, deco && classes.petal)}>
-                    <Hidden smDown>
-                        <div className={classes.topBar}>
-                            <NavLink to="/" className={classes.brand}>
-                                <img src={logo} alt={brand.name}/>
-                                Neuro Vision
-                            </NavLink>
-                        </div>
-                    </Hidden>
-                    <Typography variant="h4" className={classes.title} gutterBottom>
-                        Log In
-                    </Typography>
-                    <Typography variant="caption" className={classes.subtitle} gutterBottom align="center">
-                        Integration platform for video surveillance
-                    </Typography>
-                    <section className={classes.socmedLogin}>
-                        <div className={classes.btnArea}>
-                            <Button variant="outlined" size="small" className={classes.redBtn} type="button">
-                                <AllInclusive className={classNames(classes.leftIcon, classes.iconSmall)}/>
-                                With Google
-                            </Button>
-                            <Button variant="outlined" size="small" className={classes.blueBtn} type="button">
-                                <Brightness5 className={classNames(classes.leftIcon, classes.iconSmall)}/>
-                                With Facebook
-                            </Button>
-                        </div>
-                        <ContentDivider content="Or sign in with email"/>
-                    </section>
-                    <section className={classes.formWrap}>
-                        <form onSubmit={handleSubmit}>
-                            <div>
-                                <FormControl className={classes.formControl}>
-                                    <Field
-                                        name="email"
-                                        component={TextFieldRedux}
-                                        placeholder="Your Email"
-                                        label="Your Email"
-                                        required
-                                        className={classes.field}
-                                        value={email}
-                                        onChange={onChangeEmail}
-                                    />
-                                </FormControl>
-                            </div>
-                            <div>
-                                <FormControl className={classes.formControl}>
-                                    <Field
-                                        name="password"
-                                        component={TextFieldRedux}
-                                        onChange={onChangePassword}
-                                        value={password}
-                                        type={showPassword ? 'text' : 'password'}
-                                        label="Your Password"
-                                        InputProps={{
-                                            // endAdornment: (
-                                                // <InputAdornment position="end">
-                                                //     <IconButton
-                                                //         aria-label="Toggle password visibility"
-                                                //         onClick={this.handleClickShowPassword}
-                                                //         onMouseDown={this.handleMouseDownPassword}
-                                                //     >
-                                                //         {showPassword ? <VisibilityOff/> : <Visibility/>}
-                                                //     </IconButton>
-                                                // </InputAdornment>
-                                            // )
-                                        }}
-                                        required
-                                        validate={required}
-                                        className={classes.field}
-                                    />
-                                </FormControl>
-                            </div>
-                            <div className={classes.optArea}>
-                                {/*<FormControlLabel className={classes.label}*/}
-                                {/*                  control={<Field name="checkbox" component={CheckboxRedux}/>}*/}
-                                {/*                  label="Remember"/>*/}
-                                {/*<Button size="small" component={LinkBtn} to="/reset-password"*/}
-                                {/*        className={classes.buttonLink}>Forgot Password</Button>*/}
-                            </div>
-                            <div className={classes.btnArea}>
-                                <Button variant="contained" color="primary" size="large" type="submit">
-                                    Continue
-                                    <ArrowForward className={classNames(classes.rightIcon, classes.iconSmall)}
-                                                  disabled={submitting || pristine}/>
-                                </Button>
-                            </div>
-                        </form>
-                    </section>
-                </Paper>
-            </Fragment>
-        );
-    }
+  render() {
+    const {
+      classes,
+      handleSubmit,
+      pristine,
+      submitting,
+      deco,
+      onChangeEmail,
+      onChangePassword,
+      password,
+      email
+    } = this.props;
+    const { showPassword } = this.state;
+    return (
+      <Fragment>
+        <Hidden mdUp>
+          <NavLink to="/" className={classNames(classes.brand, classes.outer)}>
+            <img src={logo} alt={brand.name}/>
+            Neuro Vision
+          </NavLink>
+        </Hidden>
+        <Paper className={classNames(classes.paperWrap, deco && classes.petal)}>
+          <Hidden smDown>
+            <div className={classes.topBar}>
+              <NavLink to="/" className={classes.brand}>
+                <img src={logo} alt={brand.name}/>
+                Neuro Vision
+              </NavLink>
+            </div>
+          </Hidden>
+          <Typography variant="h4" className={classes.title} gutterBottom>
+            Log In
+          </Typography>
+          <Typography variant="caption" className={classes.subtitle} gutterBottom align="center">
+            Integration platform for video surveillance
+          </Typography>
+          <section className={classes.socmedLogin}>
+            <div className={classes.btnArea}>
+              <Button variant="outlined" size="small" className={classes.redBtn} type="button">
+                <AllInclusive className={classNames(classes.leftIcon, classes.iconSmall)}/>
+                With Google
+              </Button>
+              <Button variant="outlined" size="small" className={classes.blueBtn} type="button">
+                <Brightness5 className={classNames(classes.leftIcon, classes.iconSmall)}/>
+                With Facebook
+              </Button>
+            </div>
+            <ContentDivider content="Or sign in with email"/>
+          </section>
+          <section className={classes.formWrap}>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <FormControl className={classes.formControl}>
+                  <Field
+                    name="email"
+                    component={TextFieldRedux}
+                    placeholder="Your Email"
+                    label="Your Email"
+                    required
+                    className={classes.field}
+                    value={email}
+                    onChange={onChangeEmail}
+                  />
+                </FormControl>
+              </div>
+              <div>
+                <FormControl className={classes.formControl}>
+                  <Field
+                    name="password"
+                    component={TextFieldRedux}
+                    onChange={onChangePassword}
+                    value={password}
+                    type={showPassword ? 'text' : 'password'}
+                    label="Your Password"
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="Toggle password visibility"
+                            onClick={this.handleClickShowPassword}
+                            onMouseDown={this.handleMouseDownPassword}
+                          >
+                            {showPassword ? <VisibilityOff/> : <Visibility/>}
+                          </IconButton>
+                        </InputAdornment>
+                      )
+                    }}
+                    required
+                    validate={required}
+                    className={classes.field}
+                  />
+                </FormControl>
+              </div>
+              <div className={classes.btnArea}>
+                <Button variant="contained" color="primary" size="large" type="submit">
+                  Continue
+                  <ArrowForward className={classNames(classes.rightIcon, classes.iconSmall)}
+                                disabled={submitting || pristine}/>
+                </Button>
+              </div>
+            </form>
+          </section>
+        </Paper>
+      </Fragment>
+    );
+  }
 }
 
 LoginForm.propTypes = {
-    classes: PropTypes.object.isRequired,
-    handleSubmit: PropTypes.func.isRequired,
-    pristine: PropTypes.bool.isRequired,
-    submitting: PropTypes.bool.isRequired,
-    deco: PropTypes.bool.isRequired,
+  classes: PropTypes.object.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  pristine: PropTypes.bool.isRequired,
+  submitting: PropTypes.bool.isRequired,
+  deco: PropTypes.bool.isRequired,
 };
 
 const LoginFormReduxed = reduxForm({
-    form: 'immutableExample',
-    enableReinitialize: true,
+  form: 'immutableExample',
+  enableReinitialize: true,
 })(LoginForm);
 
 const reducerLogin = 'login';
 const reducerUi = 'ui';
 const FormInit = connect(
-    state => ({
-        force: state,
-        initialValues: state.getIn([reducerLogin, 'usersLogin']),
-        deco: state.getIn([reducerUi, 'decoration'])
-    }),
+  state => ({
+    force: state,
+    initialValues: state.getIn([reducerLogin, 'usersLogin']),
+    deco: state.getIn([reducerUi, 'decoration'])
+  }),
 )(LoginFormReduxed);
 
 export default withStyles(styles)(FormInit);
